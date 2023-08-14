57508b741704243450fa83438a31459bb32d543b
mayanksharma2410 <mayank.sharma2410@gmail.com>
2023-08-14 23:48:05 +0530

    Data Ingestion

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Text-Summarizer
+# End to End Text-Summarizer
+
+## Workflow
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update configuration manager in src config
+5. Update Components
+6. Update Pipeline
+7. Update main.py
+8. Update app.py
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,11 @@
+# Entity : Return type of the function for the data ingestion class
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
